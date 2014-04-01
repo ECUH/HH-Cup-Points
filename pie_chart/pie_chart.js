6e8250d9ad9f4c8a0d29bcd903e42f046fc904c1
Kenny Luong <luong97@hawaii.edu>
2014-03-31 21:24:12 -1000

    Added pie chart

---
+++ pie_chart/pie_chart.js
@@ -0,0 +1,38 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'ECUH Holmes Hall Cup Points'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'ECUH Holmes Hall Cup Points',
+            data: [
+                ['Civil/Environment',   353],
+                ['Electrical/Computer',       365],
+                ['Mechanical',       377]
+            ]
+        }]
+    });
+});
+    
+
